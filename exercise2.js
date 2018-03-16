255746f9c2439ce4c0f3063cb2090716e20daa49
violerine <viosefferine@gmail.com>
2018-03-16 23:47:44 +0700

    angkot

--- exercise2.js
+++ exercise2.js
@@ -2,25 +2,23 @@
 function naikAngkot(arrPenumpang) {
     rute = ['A', 'B', 'C', 'D', 'E', 'F'];
     var input=[['Dimitri', 'B', 'F'], ['Icha', 'A', 'B']]
-    var arrayBaru=[]
+    var arrayJarak=[]
 
     var array=[]
     for(var i=0; i<arrPenumpang.length; i++){
        array.push({penumpang:arrPenumpang[i][0],naikDari:arrPenumpang[i][1],tujuan:arrPenumpang[i][2],bayar:0})
     }
 
-    for(var j=0; j<arrPenumpang.length; j++){
+    for(var j=0; j<array.length; j++){
       var mulai= rute.indexOf(input[j][1])
       var tujuan=rute.indexOf(input[j][2])
       var totalJarak=tujuan-mulai
-      console.log(totalJarak)
-      for(var k=0; k<array.length; k++){
-        console.log("tessss"+array[k])
-        array[k].bayar+=(2000*totalJarak)
-      }
-   
+      arrayJarak.push(totalJarak)
+      console.log(arrayJarak)
+    }
     
-      
+    for(var k=0; k<arrayJarak.length; k++){
+      array[k].bayar=arrayJarak[k]*2000
     }
 
     return array
